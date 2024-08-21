89ff0db523c4e16956bffecd7574a1f532a6f5db
Raphael Brand <rbr@objektkultur.de>
2024-08-21 14:54:38 +0200

    adjust the telescope file browser this time it works

--- init.lua
+++ init.lua
@@ -159,6 +159,9 @@ require('lazy').setup({
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+
+      -- File Browser extension
+      { 'nvim-telescope/telescope-file-browser.nvim' },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
