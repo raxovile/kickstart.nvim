1342c5ea9eb8e47338de1eb5fd2dddfe08b088df
raphael <raphael.brand@gmx.net>
2024-09-05 21:16:15 +0200

    add ripgrep plugin

--- init.lua
+++ init.lua
@@ -39,6 +39,7 @@ require 'lazyInstallation'
 require('lazy').setup({
   { 'tpope/vim-sleuth' },
   { 'folke/lazy.nvim' },
+  { 'BurntSushi/ripgrep'}
   { 'nvim-lua/plenary.nvim' },
   { 'mfussenegger/nvim-dap' },
   { 'rcarriga/nvim-dap-ui' },
