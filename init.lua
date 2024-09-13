4e5091b74d89b67bf94e64117831e0ee27f260f9
Raphael Brand <rbr@objektkultur.de>
2024-09-13 09:11:14 +0200

    at least i dont get a error message anymore

--- init.lua
+++ init.lua
@@ -353,23 +353,10 @@ require('lazy').setup({
           end
         end,
       })
-
-      -- LSP servers and clients are able to communicate to each other what features they support.
-      --  By default, Neovim doesn't support everything that is in the LSP specification.
-      --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-      --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-      -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-      --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+      local tools_path = os.getenv 'TOOLS'
       local servers = {
         -- clangd = {},
         -- gopls = {},
@@ -397,6 +384,15 @@ require('lazy').setup({
               -- diagnostics = { disable = { 'missing-fields' } },
             },
           },
+        },
+        omnisharp = {
+          cmd = { 'C:/Users/RaphaelBrand/tools/omnisharp-roslyn-1.39.12/build', '--languageserver', '--hostPID', tostring(vim.fn.getpid()) },
+          filetypes = { 'cs' },
+          root_dir = require('lspconfig').util.root_pattern('*.sln', '*.csproj'),
+          capabilities = capabilities,
+          on_attach = function(client, bufnr)
+            require('lsp-status').on_attach(client, bufnr)
+          end,
         },
       }
 
@@ -649,7 +645,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'c_sharp' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
